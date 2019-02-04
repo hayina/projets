064a8d282ad800cf1c7dcf2ff50083ee74dbd48c
youssef <youssef.lechqar@gmail.com>
2019-02-05 00:00:58 +0100

    autocomplete ok

--- src/components/forms/form-fields/AutoComplete.js
+++ src/components/forms/form-fields/AutoComplete.js
@@ -18,21 +18,24 @@ const suggestions = [
     "Wetlands"
 ]
 
+
 const AutoComplete = (props) => {
 
     const [state, setState] = useState({
         filtredSuggestions: [],
         showSuggestions: false,
+        activeSuggestion: -1,
         term: ''
     })
 
     const inputEl = useRef(null);
 
+    //using the custom hook
     useClickOutside(inputEl.current, () => {
         setState({ ...state, showSuggestions: false });
     });
 
-    //user hitting input
+    //user interaction with input
     function onChange(e) {
 
         const term = e.target.value;
@@ -45,38 +48,82 @@ const AutoComplete = (props) => {
             showSuggestions = true;
         }
 
-        setState({ filtredSuggestions, showSuggestions, term });
+        setState({ ...state, filtredSuggestions, showSuggestions, term, activeSuggestion: -1 });
     }
 
     function onFocus(e) {
         onChange(e);
     }
 
+    const onKeyDown = (e) => {
+
+        const { filtredSuggestions, activeSuggestion, showSuggestions } = state;
+         
+
+        //ENTER
+        if(e.keyCode === 13) {            
+            setState({ ...state, showSuggestions: false, activeSuggestion: -1, term: filtredSuggestions[activeSuggestion] });
+        }
+        //UP
+        else if(e.keyCode === 38) {  
+
+            if(activeSuggestion > 0 )
+                setState({ ...state, activeSuggestion: activeSuggestion-1, term: filtredSuggestions[activeSuggestion-1] });
+        }
+        //DOWN
+        else if(e.keyCode === 40) {            
+
+            if( activeSuggestion < filtredSuggestions.length-1 )
+                setState({ ...state, activeSuggestion: activeSuggestion+1, term: filtredSuggestions[activeSuggestion+1] });
+        }
+
+    }
 
     // click on suggestion
-    function onClick(e) {
+    function onClick(e, suggestion) {
 
         setState({
+            ...state,
             filtredSuggestions: [],
             showSuggestions: false,
-            term: e.target.innerText
+            activeSuggestion: -1,
+            term: suggestion
         })
     }
 
+    // hover  suggestion
+    const onMouseEnter = (e, activeSuggestion) => {
+        setState({ ...state, activeSuggestion });
+    }
+
+    // leaving suggestions list
+    const onMouseLeave = (e) => {
+        setState({ ...state, activeSuggestion: -1 });
+    }
+
+    
     function renderSuggestionsList() {
 
-        const { showSuggestions, filtredSuggestions } = state;
+        const { showSuggestions, filtredSuggestions, activeSuggestion } = state;
         let suggestionsList;
 
         if (showSuggestions) {
             if (filtredSuggestions.length > 0) {
-                suggestionsList = filtredSuggestions.map((suggestion) => (
-                    <li onClick={onClick} className="suggestions-item" key={suggestion}>{suggestion}</li>
+                suggestionsList = filtredSuggestions.map((suggestion, i) => (
+                    <li 
+                        onClick={(e) => onClick(e, suggestion)} 
+                        onMouseEnter={(e) => onMouseEnter(e, i)}
+                        className={`suggestions-item ${ (i === activeSuggestion) ? 'active-suggestion':'' }`}
+                        key={suggestion}
+                    >
+                        {suggestion}
+                        {/* , i = {i}, activeSuggestion = {activeSuggestion} */}
+                    </li>
                 ))
             } else {
                 suggestionsList = (<li className="no-suggestions">No results found</li>)
             }
-            return (<ul className="suggestions-list">{suggestionsList}</ul>)
+            return (<ul className="suggestions-list" onMouseLeave={onMouseLeave}>{suggestionsList}</ul>)
         }
 
     }
@@ -88,6 +135,7 @@ const AutoComplete = (props) => {
             <input type="text" className="autocomplete-input"
                 onChange={onChange} 
                 onFocus={onFocus}
+                onKeyDown={onKeyDown}
                 value={state.term} />
             {renderSuggestionsList()}
         </div>
